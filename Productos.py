5eee92cb79b8ad4376cf0cfec1feae4ad731060f
IsraelLuna2105 <im231517@itsatlixco.edu.mx>
2026-09-17 09:24:26 -0600

    Add files via upload

--- Productos.py
+++ Productos.py
@@ -4,6 +4,7 @@ Created on Wed Sep 16 19:27:19 2026
 
 @author: isra2
 """
+#diccionario con los productos disponibles en la tienda
 InvProd = {"ID01": {"NomProd": "CerealMaiz", "Categoria": "Cereales", "Cantidad": 20, "Precio": 38},
            "ID02": {"NomProd": "CafeSoluble", "Categoria": "Cafe", "Cantidad": 15, "Precio": 50},
            "ID03": {"NomProd": "PapelHigG", "Categoria": "Higiene", "Cantidad": 5, "Precio": 46},
@@ -28,12 +29,15 @@ InvProd = {"ID01": {"NomProd": "CerealMaiz", "Categoria": "Cereales", "Cantidad"
            "ID023": {"NomProd": "Cerveza", "Categoria": "Bebidas", "Cantidad": 3, "Precio": 50},
            }
 
+#para agregar un producgto
 def agregar_producto(Productos, id_producto, Nombre, Categoria, Cantidad, Precio):
+    "Agregar producto al inventario"
     if id_producto in Productos:
         print (f"el {id_producto} ya esta")
     else: Productos[id_producto] = {"NomPrdo": Nombre, "Categoria": Categoria, "Cantidad": Cantidad, "Precio": Precio}
     print (f"Producto '{Nombre}' agregado")
     
+#para eliminar un producto
 def eliminar_producto(Productos, id_producto):
     if id_producto in Productos:
         Producto_borrado = Productos[id_producto]["NomProd"]
