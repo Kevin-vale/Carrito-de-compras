10035c398437f4d094081f7d745cdcd6e2d080db
EL_black <reyescontrerasfrancisco@gmail.com>
2026-09-17 20:48:32 -0600

    cambio final

--- Descuento.py
+++ Descuento.py
@@ -16,11 +16,11 @@ def aplicar_descuento(subtotal, tipo_descuento):
         return 0.0
 
     if tipo_descuento == "10%":
-        total = subtotal * 0.90
+        total = subtotal * 0.90 # se aplica el 10%
     elif tipo_descuento == "20%":
-        total = subtotal * 0.80
+        total = subtotal * 0.80 # se aplica el 20 %
     elif tipo_descuento == "50%":
-        total = subtotal * 0.50
+        total = subtotal * 0.50 # se aplica el 50%
     else:
         total = subtotal
 
